0d3829ddaca51c896a253c8a1f1fd3a3fb215687
Arni Magnusson <thisisarni@gmail.com>
2026-09-18 14:32:38 +1100

    Declare data and software

--- TAF/boot/model_results.R
+++ TAF/boot/model_results.R
@@ -1,1 +1,20 @@
-untar("../../../../MFCL/End.tar.gz")
+library(TAF)
+
+# Download
+zipfile <- "bet-2026-diagnostic-standalone.zip"
+url <- file.path("https://github.com/PacificCommunity",
+                 "ofp-sam-bet-2026-diagnostic/releases/download",
+                 "diagnostic-standalone-2026.08.11", zipfile)
+download.file(url)
+
+# Unzip
+taf.unzip(zipfile, junkpaths=TRUE)
+
+# Copy key files to boot/data
+key <- c("11.par", "bet.age_length", "bet.frq", "bet.ini", "bet.reg_scaling",
+         "bet.tag", "BUILD-INFO.txt", "catch.rep", "doitall.sh", "indepvar.rpt",
+         "length.fit", "mfcl.cfg", "mfclo64", "plot-11.par.rep",
+         "PROVENANCE.md", "README.txt", "run-final",
+         "standalone-final-evaluation.log", "temporary_tag_report",
+         "test_plot_output")
+cp(key, "..")
